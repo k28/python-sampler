787e3d1678c3c3dd91091514d5aab29155f1db07
Kazuya Hatano <k28@me.com>
2021-01-23 15:50:32 +0900

    複数サイトチェックできるように更新
    - 設定情報をjsonファイルから読み込む形式に変更

--- check_html_update.py
+++ check_html_update.py
@@ -11,21 +11,37 @@ import bs4
 # resuests, bs4はpipでインストールしておきます
 #
 
-# SlackのIncoming webhookのURL
-WEB_HOOK_URL = ""
-# 更新確認するサイト
-CHECK_URL = 'https://.....'
+# Webサイトの更新チェックを行うサイトなどの設定ファイルのパス
+ACCESS_SITE_INFO_PATH = "/var/tmp/check_sites_info.json"
 
-def post_message_to_slack():
-    requests.post(WEB_HOOK_URL, data = json.dumps({
-        'text': u'Slackに投稿するメッセージを入れます' + CHECK_URL
+# 設定ファイルは以下の書式
+# {
+#   "sites_info" : [
+#       { "url": "http://チェックするサイトのURL", "temp_file_name" : "更新履歴を保持するファイル名", "message" : "更新時のメッセージ"},
+#       ... 複数サイト指定可能, tmp_file_nameはサイト毎に分ける
+#   ],
+#   "web_hook_url" : "http://.... SlackのIncommingWebhookのURL"
+# }
+#
+
+# 更新チェックを行うサイトのリストを取得する
+def load_check_site_info():
+    info_path = ACCESS_SITE_INFO_PATH
+    f = open(info_path, 'r')
+    load_info = json.load(f)
+    
+    return load_info
+
+# Slackにメッセージを投稿します
+def post_message_to_slack(url, message):
+    requests.post(url, data = json.dumps({
+        'text': u'' + message
     }))
 
-def check_is_website_update():
-    # チェックするURL
-    url = CHECK_URL
+# urlのサイトが更新されているか調べる. 結果はtmpファイルに保存する
+def check_is_website_update(url, tmp):
     # URLの結果を保存しておくファイル(次回の比較に使用する)
-    file = '/tmp/html_source.txt'
+    file = '/tmp/' + tmp
     
     res = requests.get(url)
     res.raise_for_status()
@@ -52,9 +68,13 @@ def check_is_website_update():
 
 
 def main():
-    if (check_is_website_update()):
-        # slackに通知する
-        post_message_to_slack()
+
+    check_sites_info = load_check_site_info()
+    web_hook_url = check_sites_info["web_hook_url"]
+
+    for site in check_sites_info["sites_info"]:
+        if (check_is_website_update(site['url'], site['temp_file_name'])):
+            post_message_to_slack(web_hook_url, site['message'] + site['url'])
 
 if __name__ == '__main__':
     main()
